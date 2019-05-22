5847b562b135eb47dbd0978e4dbe8a6fd30c306e
Parker Helland <hellandp@gmail.com>
2019-05-22 15:58:48 -0700

    refactored using ACTIONS file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import { createStore } from 'redux';
+import {
+  ADD_DRINK,
+  REMOVE_DRINK,
+  ADD_CHIPS,
+  REMOVE_CHIPS,
+  ADD_SANDWICH,
+  REMOVE_SANDWICH,
+  removeChips,
+  removeDrink,
+  removeSandwich,
+  addChips,
+  addDrink,
+  addSandwich,
+  ADD_FULL_BOAT,
+  addFullBoat,
+  REMOVE_FULL_BOAT,
+  removeFullBoat
+} from './actions/lunchActions';
 
 const initialState = {
   drink: null,
@@ -9,24 +27,24 @@ const initialState = {
 function reducer(state = initialState, action) {
   
   switch(action.type) {
-    case 'ADD_DRINK':
+    case ADD_DRINK:
       return { ...state, drink: action.payload };
-    case 'REMOVE_DRINK':
+    case REMOVE_DRINK:
       return { ...state, drink: null };
-    case 'ADD_CHIPS':
+    case ADD_CHIPS:
       return { ...state, chips: action.payload };
-    case 'REMOVE_CHIPS':
+    case REMOVE_CHIPS:
       return { ...state, chips: null };
-    case 'ADD_SANDWICH':
+    case ADD_SANDWICH:
       return { ...state, sandwich: action.payload };
-    case 'REMOVE_SANDWICH':
+    case REMOVE_SANDWICH:
       return { ...state, sandwich: null };
-    case 'ADD_FULL_BOAT':
+    case ADD_FULL_BOAT:
       return { ...state, 
         drink: action.payload.drink, 
         chips: action.payload.chips, 
         sandwich: action.payload.sandwich };
-    case 'REMOVE_FULL_BOAT':
+    case REMOVE_FULL_BOAT:
       return { ...state,
         drink: null, 
         chips: null, 
@@ -38,40 +56,29 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({
-  type: 'ADD_DRINK',
-  payload: 'pepsi'
-});
+// ADD + REMOVE DRINKS
+store.dispatch(addDrink('COKE'));
 console.log('add drink', store.getState());
-
-store.dispatch({
-  type: 'REMOVE_DRINK',
-  payload: null
-});
+store.dispatch(removeDrink());
 console.log('remove drink', store.getState());
 
-store.dispatch({
-  type: 'ADD_CHIPS',
-  payload: 'lays'
-});
+// ADD + REMOVE CHIPS
+store.dispatch(addChips('LAYS'));
 console.log('add chips', store.getState());
+store.dispatch(removeChips());
+console.log('remove chips', store.getState());
 
-store.dispatch({
-  type: 'REMOVE_CHIPS',
-  payload: null
-});
-console.log('remove drink', store.getState());
+// ADD + REMOVE SANDWICH
+store.dispatch(addSandwich('SUB'));
+console.log('add sandwich', store.getState());
+store.dispatch(removeSandwich());
+console.log('remove sandwich', store.getState());
 
-store.dispatch({
-  type: 'ADD_FULL_BOAT',
-  payload: 
-    { drink: 'pepsi', chips: 'lays', sandwich: 'sub' }
-});
+// ADD + REMOVE FULL BOAT
+store.dispatch(addFullBoat({ 
+  drink: 'pepsi', 
+  chips: 'lays',
+  sandwich: 'sub' }));
 console.log('add full boat', store.getState());
-
-store.dispatch({
-  type: 'REMOVE_FULL_BOAT',
-  payload: 
-  { drink: null, chips: null, sandwich: null }
-});
+store.dispatch(removeFullBoat());
 console.log('remove full boat', store.getState());
