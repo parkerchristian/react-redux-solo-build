715245c13dca5346ad3a72ba1bcbe2529684c5ba
Parker Helland <hellandp@gmail.com>
2019-05-23 12:07:53 -0700

    combineReducer used in index.js updated root index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import { createStore } from 'redux';
-import reducer from './reducer/postReducer';
-import { createPost, deletePost, deleteOnePost, updatePost } from './actions/postActions';
+import reducer from './reducer/index';
+import { createComment } from './actions/commentActions';
+import { createPost } from './actions/postActions';
 
 const store = createStore(reducer);
 
-store.dispatch(createPost(
-  'Sup', 'We are Words and words and stuff'
-));
-store.dispatch(createPost(
-  'Again', '.......body......'
-));
-console.log('POSTS', store.getState());
+store.dispatch(createPost('TITLE', 'Body'));
+store.dispatch(createComment(1, 'comment on post'));
 
-store.dispatch(updatePost(1, 'new updated body'));
-console.log('UPDATE', store.getState());
-
-store.dispatch(deleteOnePost(0));
-console.log('DELETE_ONE', store.getState());
-store.dispatch(deletePost());
-console.log('DELETE', store.getState());
+console.log('COMMENT', store.getState());
