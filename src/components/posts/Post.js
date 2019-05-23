ac02bcd14f08311321d2673ba66f2ef1f6c0b7f5
Parker Helland <hellandp@gmail.com>
2019-05-23 16:12:18 -0700

    rendering Post.js on local host

---
+++ src/components/posts/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Post() {
+  return (
+    <>
+      <h1>SUP</h1>
+    </>
+  );
+}
+
+
+
+export default Post;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import { createStore } from 'redux';
-import reducer from './reducer/index';
-import { createComment } from './actions/commentActions';
-import { createPost } from './actions/postActions';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/App';
 
-const store = createStore(reducer);
 
-store.dispatch(createPost('TITLE', 'Body'));
-store.dispatch(createPost('Post 2', 'Second body'));
-store.dispatch(createComment(1, 'comment'));
-store.dispatch(createComment(1, 'comment on'));
-store.dispatch(createComment(1, 'comment on post'));
-store.dispatch(createComment(2, 'comment on second post'));
-store.dispatch(createComment(2, 'comment two on second post'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-console.log('COMMENT', store.getState());
+// import { createStore } from 'redux';
+// import reducer from './reducer/index';
+// import { createComment } from './actions/commentActions';
+// import { createPost } from './actions/postActions';
+
+// const store = createStore(reducer);
+
+// store.dispatch(createPost('TITLE', 'Body'));
+// store.dispatch(createPost('Post 2', 'Second body'));
+// store.dispatch(createComment(1, 'comment'));
+// store.dispatch(createComment(1, 'comment on'));
+// store.dispatch(createComment(1, 'comment on post'));
+// store.dispatch(createComment(2, 'comment on second post'));
+// store.dispatch(createComment(2, 'comment two on second post'));
+
+// console.log('COMMENT', store.getState());
