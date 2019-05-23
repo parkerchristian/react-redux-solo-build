9d8e2856d3d80948819e85fac2d1803635a5f234
Parker Helland <hellandp@gmail.com>
2019-05-22 17:04:36 -0700

    blog-actions working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  posts: []
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'POST':
+      return { ...state, posts: [...state.posts, { title: action.payload.title,
+        body: action.payload.body }
+      ] };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'POST',
+  payload: { title: 'Hey! SUCKA', body: 'Wordy words words' }
+});
+console.log('created POST', store.getState());
