3e53cea948c67e5078badf11e451cae26faf0d0a
Parker Helland <hellandp@gmail.com>
2019-05-23 10:24:49 -0700

    test

--- src/reducer/postReducer.test.js
+++ src/reducer/postReducer.test.js
@@ -1,5 +1,12 @@
+import reducer from './postReducer';
+import { createPost } from '../actions/postActions';
+
 describe('Post REDUCER', () => {
   it('handle the create post action', () => {
-    expect().toEqual();
+    const newState = reducer([], createPost('TITLE', 'Body'));
+
+    expect(newState).toEqual([
+      { title: 'TITLE', body: 'Body' }
+    ]);
   });
 });
