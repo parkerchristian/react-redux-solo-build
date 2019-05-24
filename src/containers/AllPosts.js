a28d4550ad8f96773440ad2d0f1ff08404b9401e
Parker Helland <hellandp@gmail.com>
2019-05-23 17:10:11 -0700

    post containers

---
+++ src/containers/AllPosts.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { getPosts } from '../selectors/postSelectors';
+import Posts from '../components/posts/Posts';
+
+const mapStateToProps = state => ({
+  posts: getPosts(state)
+});
+
+export default connect(
+  mapStateToProps,
+  null
+)(Posts);
