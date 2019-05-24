cf98cb57eed39630b62192bf5a35ffecdb24bad7
Parker Helland <hellandp@gmail.com>
2019-05-24 15:05:27 -0700

    comment form renders..

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CreatePost from '../containers/CreatePost';
 import AllPosts from '../containers/AllPosts';
+import CommentForm from './comments/CommentForm';
 
 export default function App() {
   return (
     <>
       <CreatePost />
+      <CommentForm />
       <AllPosts />
     </>
   );
