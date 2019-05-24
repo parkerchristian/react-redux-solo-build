5e2aeb1679b2c664e26c326a10aa270a6edd5cf9
Parker Helland <hellandp@gmail.com>
2019-05-24 14:18:31 -0700

    Comments.js

---
+++ src/components/comments/Comments.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+
+function Comments({ comments }) {
+  const commentList = comments.map(comment => {
+    return (
+      <li key={comment.id}>
+        <Comment comment={comment} />
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      {commentList}
+    </ul>
+  );
+}
+
+Comments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Comments;
